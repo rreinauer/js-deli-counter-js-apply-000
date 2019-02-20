ddbdd84b5d6aa45e0bde96151783e565703a49ab
Rachel Reinauer <rereinauer@gmail.com>
2019-02-20 17:25:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ function takeANumber(current, name) {
   current.push(name);
   return `Welcome, ${name}. You are number ${current.length} in line.`
 }
+var n = 0;
+function takeANumberAlt(current) {
+  n++;
+  current.push(n);
+  return n;
+}
+function resetCounter() {
+  n = 0;
+  
+}
+
+
 function nowServing(katzDeliLine) {
   if(katzDeliLine.length === 0) {
     return 'There is nobody waiting to be served!'
